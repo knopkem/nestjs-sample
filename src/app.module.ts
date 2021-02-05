e27423824cd9b989ba75f20026ee23633f39dab5
Michael Knopke <knopkem@gmail.com>
2021-02-05 12:08:32 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DogsModule } from './dogs/dogs.module';
-import { DefaultAdminModule } from 'nestjs-admin';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), DogsModule, DefaultAdminModule],
+  imports: [TypeOrmModule.forRoot(), DogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
